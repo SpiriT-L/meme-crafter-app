60698255db26318ef92cb80a836910f535ff659a
LeonID <lmatskevich79@gmail.com>
2025-05-02 00:17:47 +0300

    feat: customized eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      react: require('eslint-plugin-react'),
+      reactHooks: require('eslint-plugin-react-hooks'),
+      jsxAlly: require('eslint-plugin-jsx-a11y'),
+    },
+    files: ['*.js', '*.ts', '*.tsx', '*.jsx'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'no-unused-vars': 'warn',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/no-unknown-property': 'warn',
+      'jsx-a11y/anchor-is-valid': 'warn',
+      'react/jsx-no-constructed-context-values': 'warn',
+      'react/no-adjacent-inline-elements': 'warn',
+    },
+  },
+  {
+    ignores: [
+      'node_modules',
+      'build',
+      'dist',
+      'coverage',
+      '.next',
+      '.turbo',
+      '.vercel',
+      '.github',
+    ],
+  },
+];
+
+export default eslintConfig;
